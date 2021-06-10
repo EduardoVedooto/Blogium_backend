d6151ab360a286260a51eec1e0843ff46df9d9f0
Eduardo Vedooto <vedootoeduardo@gmail.com>
2021-06-10 09:15:15 -0300

    Added get.comments

--- src/App.js
+++ src/App.js
@@ -15,7 +15,12 @@ App.get("/posts", (req, res) => {
 
 App.post("/posts", (req, res) => {
     const data = Load(filePath);
-    data.posts.push({ ...req.body, "id": data.posts.length + 1 });
+    data.posts.push({
+        ...req.body,
+        "id": data.posts.length + 1,
+        "commentCount": 0,
+        "comments": []
+    });
     Save(data, filePath);
     res.send("Success");
 });
@@ -24,6 +29,11 @@ App.get("/posts/:id", (req, res) => {
     const id = req.params.id;
     const data = Load(filePath);
     res.send(data.posts.find(item => item.id === parseInt(id)));
+});
+
+App.get("/posts/:id/comments", (req, res) => {
+    const data = Load(filePath);
+    res.send(data.comments.filter(comment => comment.postId === parseInt(req.params.id)));
 });
 
 App.listen(3333, () => console.log("Running server..."));
